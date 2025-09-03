3f56faf393c4563f722c64ccd1115680f046616f
Aviwer Nigam <aviwer20041228@gmail.com>
2025-09-03 23:15:23 +0530

    Admin dashboard pager added

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,36 @@
 // Server/server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+let reports = [];
+
+// 1. Serve static files
+app.use(express.static(path.join(__dirname, "../Client")));
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('../Client')); // serve frontend files
 
-// For testing server
-app.get('/api', (req, res) => {
-    res.send('Server is running');
+// 2. Serve index.html at root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Client/login.html"));
 });
 
-// Start server
-const PORT =3000;
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+app.get("/new-issue",(req,res)=>{
+  res.sendFile(path.join(__dirname, "../Client/report.html"));
+})
+
+app.get("/user",(req,res)=>{
+  res.sendFile(path.join(__dirname, "../Client/userui.html"));
+})
+// 3. Start server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT})`));
